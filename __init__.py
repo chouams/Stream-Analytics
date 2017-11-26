7d3f17c8ce1d29db2a00cabc74eca2f5ae957e9a
PaulBK3 <33777830+PaulBK3@users.noreply.github.com>
2017-11-26 21:48:16 +0100

    add __init__
    script running on Server atm

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return "Dies ist die Startseite."
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hi %s!</h1>' %name
+
+
+if __name__ == "__main__":
+    app.run()
